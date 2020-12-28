1a4602477a505efb12ccc953204687da513a4ed5
Nathan Lanza <nathan@lanza.io>
2020-12-28 17:16:55 -0500

    Sample

---
+++ Toolchain-Darwin-x86_64.cmake
@@ -0,0 +1,14 @@
+
+set(CMAKE_AR /usr/local/opt/llvm/bin/llvm-ar CACHE FILEPATH "")
+set(CMAKE_ASM_COMPILER /usr/local/opt/llvm/bin/clang CACHE FILEPATH "")
+set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang CACHE FILEPATH "")
+set(CMAKE_CXX_COMPILER /usr/local/opt/llvm/bin/clang++ CACHE FILEPATH "")
+set(CMAKE_RANLIB /usr/local/opt/llvm/bin/llvm-ranlib CACHE FILEPATH "")
+
+
+# set(CMAKE_AR /usr/bin/ar CACHE FILEPATH "")
+# set(CMAKE_ASM_COMPILER /usr/bin/clang CACHE FILEPATH "")
+# set(CMAKE_C_COMPILER /usr/bin/clang CACHE FILEPATH "")
+# set(CMAKE_CXX_COMPILER /usr/bin/clang++ CACHE FILEPATH "")
+# set(CMAKE_RANLIB /usr/bin/ranlib CACHE FILEPATH "")
+
